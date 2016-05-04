c477495a76ccf32d9b6002b96cd6e8ad6387f6b3
Zindae <thomasvp@kth.se>
2016-05-04 13:23:36 +0200

    Service & Ctrl $value working
    Service & Ctrl $value working

--- app/js/controllers/test-persistence-ctrl.js
+++ app/js/controllers/test-persistence-ctrl.js
@@ -1,8 +1,8 @@
 myApp.controller('persistenceCtrl', ['$scope','$rootScope', 'Authentication', function($scope, $rootScope, Authentication) {
 	
 	$scope.logRoot = function () {
-		console.log('logRoot: ', Authentication.RSCU);
-		console.log('logRoot w/$value: ', Authentication.RSCU.$value);
+		console.log('logRoot: ', $rootScope.currentUser.$value);
+		console.log('logRoot w/$value: ', Authentication.$value);
 		return null;
 	};
 
@@ -10,13 +10,27 @@ myApp.controller('persistenceCtrl', ['$scope','$rootScope', 'Authentication', fu
 		// console.log(Authentication.getUserData());
 		return Authentication.getUserData();
 	};
-
+	$scope.check = false;
 	$scope.addCommentToUser = function (test) {
 		// console.log(test.header, test.comment);
 		// console.log('roscp.currUsr in func: '.$rootScope.currentUser);
 		// console.log($rootScope.currentUser.$value);
-		$rootScope.currentUser.$value[test.header] = test.comment;
-		$rootScope.currentUser.$save();
+		// $rootScope.currentUser.$value = test;
+		
+		
+		//Get current data:
+		
+		console.log($scope.check);
+		if ($scope.check === false) {
+			Authentication.getCurr();
+			console.log($scope.check);
+			$scope.check = true;
+			console.log($scope.check);
+		}
+		// console.log(curr);
+		var update = Authentication.updateCurr(test);
+		console.log(update);
+		// $rootScope.currentUser.$save();
 		// console.log($rootScope.currentUser);
 		return;
 	};
